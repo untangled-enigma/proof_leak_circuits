b97a6107af42f3aadf612a8837038b42450b2a0f
Himank Jain <himankjain@yahoo.in>
2025-12-05 00:56:39 +0530

    sequence type extended

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,6 +1,13 @@
+import { CircuitInput } from "@zk-email/zkemail-nr"
 import {ProofLeakProver,ProvingBackend, generateInputs} from "./ProofLeakProver"
 import { isValidEml , extractEmailAddresses, extractEmail, domainSequence} from "./emailUtils"
+import { Sequence } from "@zk-email/zkemail-nr/dist/utils"
 
 export { ProofLeakProver, generateInputs,isValidEml,extractEmailAddresses,domainSequence, extractEmail}
 
-export type {ProvingBackend} 
+type PLCircuitInputs = CircuitInput & {
+    to_domain_sequence : Sequence;
+    from_domain_sequence: Sequence;
+}
+
+export type {ProvingBackend,PLCircuitInputs} 
