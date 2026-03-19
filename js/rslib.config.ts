089df195ee01e5df6290a0a0836173fc64e38509
Himank Jain <himankjain@yahoo.in>
2026-03-19 14:43:41 +0530

    build script optimisation

--- js/rslib.config.ts
+++ js/rslib.config.ts
@@ -3,14 +3,30 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   lib: [
     {
+      id: 'node',
       format: 'esm',
       syntax: ['node 22'],
-      dts: true,
+      dts: {
+        distPath: './dist/node',
+      },
+      output: {
+        target: 'node',
+        distPath: {
+          root: './dist/node',
+        },
+      },
+    },
+    {
+      id: 'browser',
+      format: 'esm',
+      syntax: ['chrome >= 111', 'safari >= 16.4', 'firefox >= 113', 'edge >= 111'],
+      dts: false,
+      output: {
+        target: 'web',
+        distPath: {
+          root: './dist/browser',
+        },
+      },
     },
   ],
-
-  output: {
-    target: 'web',
-  },
-
 });
